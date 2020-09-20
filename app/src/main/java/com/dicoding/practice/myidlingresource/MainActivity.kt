b67e0f6aeaf999fa17635540bc8f5776b1320d07
irshalfir <irshalfir@gmail.com>
2020-09-20 12:48:30 +0700

    modify MainActivity

--- app/src/main/java/com/dicoding/practice/myidlingresource/MainActivity.kt
+++ app/src/main/java/com/dicoding/practice/myidlingresource/MainActivity.kt
@@ -2,10 +2,23 @@ package com.dicoding.practice.myidlingresource
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            delayOne()
+        }
+
+    }
+
+    private fun delayOne() {
+        Handler().postDelayed({
+            text_view.text = getString(R.string.delay1)
+        }, 2000)
     }
 }
